d962530421f7f5b898b7da6a018a425482ba841a
Kevin Zolman <kevin.zolman@gmail.com>
2023-08-01 11:37:34 -0600

    Add 4 new arrays (friends, numbers, dec_numbers, boolean) and add 4 methods acting on these arrays

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,14 @@ numbers = [1, 2, 3, 4]
 dec_numbers = [1.0, 2.0, 3.0, 4.0]
 boolean = [true, true, false, true]
 
+# Will remove the last element (index position 3) from the friends array and returns this element. Bye Lib :(
+friends.pop
+
+# Will push the element "Lib" into the friends array in the final index position (index position 3). Hi Lib :)
+friends.push("Lib")
+
+# Will remove the first element (1) from the numbers array (index position 0) and return this value. 
+numbers.shift
+
+# Will add the integer 1 to the numbers array in index position 0. 
+numbers.unshift(1)
